bc6b763628d11f3527058e24233ad2f1812a4543
Yuan Zhao <yuanzhaoYZ@users.noreply.github.com>
2019-01-04 12:05:38 -0600

    Update control.sh

--- airflow-csd-src/scripts/control.sh
+++ airflow-csd-src/scripts/control.sh
@@ -2,7 +2,6 @@
 
 set -ex
 
-useradd airflow -g airflow -m -s /bin/bash
 chmod u+x $CONF_DIR/scripts/*.sh
 chown -R airflow: $CONF_DIR/logs
 chown -R airflow: $CONF_DIR/airflow-conf
@@ -31,30 +30,30 @@ case ${SERVICE} in
     log "Initialising the Airflow metadata DB"
     substitute_common_tokens
     substitute_webserver_tokens
-    exec su airflow -c "$AIRFLOW_CONDA_HOME/bin/airflow initdb" >> ${CONF_DIR}/logs/stdout.log 2>> ${CONF_DIR}/logs/stderr.log
+    exec su airflow -c "airflow initdb" >> ${CONF_DIR}/logs/stdout.log 2>> ${CONF_DIR}/logs/stderr.log
     ;;
   (webserver_start)
     log "Starting the Airflow webserver"
     substitute_common_tokens
     substitute_webserver_tokens
-    exec su airflow -c "$AIRFLOW_CONDA_HOME/bin/airflow webserver" >> ${CONF_DIR}/logs/stdout.log 2>> ${CONF_DIR}/logs/stderr.log
+    exec su airflow -c "airflow webserver" >> ${CONF_DIR}/logs/stdout.log 2>> ${CONF_DIR}/logs/stderr.log
     ;;
   (celery_start)
     log "Starting the Airflow celery"
     substitute_common_tokens
     substitute_celery_tokens
-    exec su airflow -c "$AIRFLOW_CONDA_HOME/bin/airflow flower" >> ${CONF_DIR}/logs/stdout.log 2>> ${CONF_DIR}/logs/stderr.log
+    exec su airflow -c "airflow flower" >> ${CONF_DIR}/logs/stdout.log 2>> ${CONF_DIR}/logs/stderr.log
     ;;
   (worker_start)
     log "Starting the Airflow worker"
     substitute_common_tokens
-    exec su airflow -c "$AIRFLOW_CONDA_HOME/bin/airflow worker -q ${WORKER__QUEUE}" >> ${CONF_DIR}/logs/stdout.log 2>> ${CONF_DIR}/logs/stderr.log
+    exec su airflow -c "airflow worker -q ${WORKER__QUEUE}" >> ${CONF_DIR}/logs/stdout.log 2>> ${CONF_DIR}/logs/stderr.log
     ;;
   (scheduler_start)
     log "Starting the Airflow scheduler"
     substitute_common_tokens
     substitute_scheduler_tokens
-    exec su airflow -c "$AIRFLOW_CONDA_HOME/bin/airflow scheduler" >> ${CONF_DIR}/logs/stdout.log 2>> ${CONF_DIR}/logs/stderr.log
+    exec su airflow -c "airflow scheduler" >> ${CONF_DIR}/logs/stdout.log 2>> ${CONF_DIR}/logs/stderr.log
     ;;
   (worker_pip_install)
     log "Installing additional python packages by pip"
